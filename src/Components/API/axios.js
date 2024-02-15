1185be46dce6724d2c834b53912c792164ef8d69
Jordy Rocacher <jmr4277@orange.fr>
2024-02-15 16:20:49 +0100

    maj url

--- src/Components/API/axios.js
+++ src/Components/API/axios.js
@@ -1,7 +1,7 @@
-/*import axios from "axios";
+import axios from "axios";
 
 export const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export default axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
-});*/
+});
