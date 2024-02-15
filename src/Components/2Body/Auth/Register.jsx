3624e32651ef7402cb2358056268e522d0341c0b
Jordy Rocacher <jmr4277@orange.fr>
2024-02-15 21:41:37 +0100

    maj url

--- src/Components/2Body/Auth/Register.jsx
+++ src/Components/2Body/Auth/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import axios from "../../API/axios";
 import { Link, useNavigate } from "react-router-dom";
 
 //regex valid
